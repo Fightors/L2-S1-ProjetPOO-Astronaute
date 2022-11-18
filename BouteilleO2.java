531949526b821c9ebc19cf0d3809fc6a9259b589
Enzo Botans <enzo.botans@etu.univ-nantes.fr>
2022-11-18 15:05:54 +0100

    creation des equipements à modifier

---
+++ BouteilleO2.java
@@ -0,0 +1,12 @@
+public class BouteilleO2 extends Equipement{
+    private int capaO2Max;
+    private int O2actuel;
+
+    public int getcapaO2Max(){
+        return this.capaO2Max;
+    }
+
+    public int O2actuel(){
+        return this.O2actuel;
+    }
+}
