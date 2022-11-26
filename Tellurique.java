96d219710e81418314aa9b19a0857de337026899
Lucas Bocquené <lucas.bocquene@etu.univ-nantes.fr>
2022-11-26 14:49:58 +0100

    delete Equipement Ajout get set Change constructeurs

--- Tellurique.java
+++ Tellurique.java
@@ -2,7 +2,7 @@ public class Tellurique extends Planete {
     //O2, vie??
 
     public Tellurique (int temp, boolean perm, float dia, float grav, Position pos ,int tO, boolean aN){
-        super(temp,perm,dia,grav,pos, tO, aN);
+        super(temp,true,dia,grav,pos, tO, false);
     }
 
 }
