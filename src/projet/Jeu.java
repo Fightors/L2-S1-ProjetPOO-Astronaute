295cfa31b69f4b87b7db268ca3fdb2252c9ae2d6
Théotime Godefroy <theotime.godefroy@etu.univ-nantes.fr>
2022-11-28 18:23:03 +0100

    Main Tour + Jeu

--- src/projet/Jeu.java
+++ src/projet/Jeu.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 public class Jeu{
     public static void main(String[] args){ 
         boolean foundIt = false;
-        Astronaute Thomas = new Astronaute(100, true, (50,50));
+        Astronaute Thomas = new Astronaute();
+        Univers universal = new Univers();
+        Tour turn = new Tour();
         while(Thomas.estVivant && !foundit){
-            
+            asn.vaisseau.scanner(universal, Thomas, 10);
             turn.jouerTour();
         }
     }
--- src/projet/Tour.java
+++ src/projet/Tour.java
@@ -14,40 +14,45 @@ public class Tour{
 
     public jouerTour(Univers univ, Astronaute asn, boolean foundIt){
         asn.seDeplacer();
-        int deathCooldown;
         CorpsCeleste corps = univ.intersection();
-        if(corps instanceof Asteroide || 
-        (corps instanceof Etoile && corps.estDansRay(asn))){
-            asn.setEstVivant(false);
-        }
-        else{
-            asn.reducO2();
-            if(asn.bouteilleO2.getO2actuel()<=0){
+        this.plusTour();
+        if(corps!=null){
+            if(corps instanceof Asteroide || 
+            (corps instanceof Etoile && corps.estDansRay(asn))){
                 asn.setEstVivant(false);
             }
             else{
-                if(corps.getPermAtt() == true){
-                    if(corps.estDangereux() == true){
-                        System.out.println("Attention ! Danger de mort sur ce corps céleste !")
-                    }
-                    else{
-                        if(corps instanceof Tellurique){
-                            if(corps.getAirNocif == true){
-                                System.out.println("Attention ! Danger de mort sur ce corps céleste !");
-                                if(corps.getTauxO2()>0){
-                                    asn.bouteilleO2.setO2actuel(asn.bouteilleO2.getcapaO2Max);
+                asn.reducO2();
+                if(asn.bouteilleO2.getO2actuel()<=0){
+                    asn.setEstVivant(false);
+                }
+                else{
+                    if(corps.getPermAtt() == true){
+                        if(corps.estDangereux() == true){
+                            System.out.println("Attention ! Danger de mort sur ce corps céleste !")
+                        }
+                        else{
+                            if(corps instanceof Tellurique){
+                                if(corps.getAirNocif == true){
+                                    System.out.println("Attention ! Danger de mort sur ce corps céleste !");
+                                    if(corps.getTauxO2()>0){
+                                        asn.bouteilleO2.setO2actuel(asn.bouteilleO2.getcapaO2Max);
+                                    }
                                 }
-                            }
-                            else{
-                                foundIt=true;
-                                System.out.println("Bravo ! Vous avez trouvé une planète vivable, il est temps de commencer votre nouvelle vie !");
-                                System.out.println("Le corps céleste vivable que vous avez trouver est :");
-                                corps.afficherCorpsCeleste();
+                                else{
+                                    foundIt=true;
+                                    System.out.println("Bravo ! Vous avez trouvé une planète vivable en "+ this.nbTours+", il est temps de commencer votre nouvelle vie !");
+                                    System.out.println("Le corps céleste vivable que vous avez trouver est :");
+                                    corps.afficherCorpsCeleste();
+                                }
                             }
                         }
                     }
                 }
             }
+        }
+        else{
+            System.out.println("Vous n'avez rencontré aucun corps céleste lors de ce tour !");
         }
     }
 }
