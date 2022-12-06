1cf3bf97f8fed81a5fdc2ac503bd5714a093af6e
Théotime Godefroy <theotime.godefroy@etu.univ-nantes.fr>
2022-12-06 15:43:24 +0100

    Update Graph + Jeu + Univers

--- src/projet/Graph.java
+++ src/projet/Graph.java
@@ -9,34 +9,88 @@ import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import java.util.ArrayList;
+
 
 public class Graph extends Application {
-    private Univers univ;
 
     @Override 
     public void start(Stage stage){
+        Univers univ = Univers.universTest();
         Group root = new Group();
-        root.getChildren().add( new Circle(this.univ.getAstronaute().getPos().getX(),this.univ.getAstronaute().getPos().getY(),5, Color.WHITE));
-        for(CorpsCeleste c : this.univ.getListeCorps()){
-            root.getChildren().add(new Circle( c.getPos().getX(), c.getPos().getY(), c.getDiam()/2, Color.RED));  
-        }
-        Scene scene = new Scene(root,1000,1000,Color.BLACK);
+        Scene s = new Scene(root, 1000, 1000, Color.BLACK);
+        Canvas canvas = this.draw(univ);
+
+        root.getChildren().add(canvas);
         stage.setTitle("Captain Planet");
-        stage.setScene(scene);
-        stage.show();
+        stage.setScene(s);
+        stage.show(); // Fonctionne jusqu'ici, bug de la fenre après quand elle s'execute en même temps qu'une demande du terminal
+        /*
+        while(univ.getAstronaute().getVivant()==true && univ.getAstronaute().getTrouvePlanete() == false){
+            ArrayList<CorpsCeleste> l_scan = univ.getAstronaute().getVaisseau().scanner(univ, univ.getAstronaute(), 100);
+            this.jouerTour(univ,univ.getAstronaute());
+            System.out.println("\n");
+        }
+        */
+    }
+
+    public Canvas draw(Univers u){
+        Canvas canvas = new Canvas(1000,1000);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillOval(u.getAstronaute().getPos().getX(),u.getAstronaute().getPos().getY(),10,10);
+        gc.setFill(Color.RED);
+        for(CorpsCeleste c : u.getListeCorps()){
+            gc.fillOval(c.getPos().getX(),c.getPos().getY(),c.getDiam()/2,c.getDiam()/2);  
+        }
+        return canvas;
+    }
+
+    public void jouerTour(Univers univ, Astronaute asn){
+        univ.moveAste();
+        System.out.println("Position de l'astronaute:" );
+        asn.getPos().affichagePos();
+        asn.seDeplacer();
+        asn.reducO2();
+        if(asn.getBouteilleO2().getO2actuel()<=0){
+            System.out.println("Vous êtes mort car votre bouteille d'oxygène est vide !");
+            asn.setEstVivant(false);
+        }
+        else{
+            CorpsCeleste corps = univ.intersectionCorpsCeleste();
+            if(corps!=null){
+                if(corps.estDangereux() == true){
+                    asn.setEstVivant(false);
+                }
+                else{
+                    if(corps.estVivable()==false){
+                        System.out.println("Cette planète n'est pas vivable, vous avez néanmoins rechargé votre bouteille d'oxygène !");
+                        asn.getBouteilleO2().setO2actuel(asn.getBouteilleO2().getcapaO2Max());
+                    }
+                    else{
+                        asn.setTrouvePlanete(true);
+                        System.out.println("Bravo ! Vous avez trouvé une planète vivable en X tour(s), il est temps de commencer votre nouvelle vie !");
+                        System.out.println("Le corps céleste vivable que vous avez trouvé est :");
+                        corps.afficherCorpsCeleste();
+                    }
+                }
+            }
+            else{
+                System.out.println("Vous n'avez rencontré aucun corps céleste lors de ce tour !");
+            }
+        } 
     }
 
     public void fermetureInterface(Stage stage){
         stage.close();
     }
-    
-
-    public Univers getUnivers(){
-        return this.univ;
-    }
 
     public static void main(String[] args){
         launch(args);
     }
-
 }
--- src/projet/Jeu.java
+++ src/projet/Jeu.java
@@ -1,57 +1,17 @@
 package projet;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.control.*;
-import javafx.stage.Stage;
-import javafx.scene.Group;
 import java.util.ArrayList;
 
 public class Jeu{
       
     public static void main(String[] args){
-        Graph graph = new Graph();
-
-        Vaisseau v1;
-        v1 = new Vaisseau(50);
-        
-        BouteilleO2 bout = new BouteilleO2(100,70);
-
-        CorpsCeleste p1,p2,p3,p4,p5,aste1,eto1;
-
-        p1 = new Tellurique(60,40,1,new Position(100,250),80,false);
-        p2 = new Tellurique(50,20,(float)0.7,new Position(400,200),80,true);
-        p3 = new Tellurique(20,20,1,new Position(250,-250),80,false);
-        p4 = new Gazeuse(0,50,0,new Position(50,-300),2);
-        p5 = new Tellurique(-50,50,1,new Position(-300,-100),50,false);
-        aste1 = new Asteroide(0,40,1,new Position(450,450),new Position(-20,-20));
-        eto1 = new Etoile(500,50,50,new Position(-200,250));
-
-        ArrayList<CorpsCeleste> lC = new ArrayList<CorpsCeleste>();
-        ArrayList<CorpsCeleste> lS;
-
-        lC.add(p3);
-        lC.add(p2);
-        lC.add(p1);
-        lC.add(aste1);
-        lC.add(eto1);
-        lC.add(p4);
-        lC.add(p5);
-
-            
-        Astronaute Thomas = new Astronaute(bout,new Position(0,0),v1);
-        
-        Univers u = new Univers(Thomas,lC);
+        Univers univ = Univers.universTest();
         Tour turn = new Tour();
 
         
-        while(Thomas.getVivant()==true && Thomas.getTrouvePlanete() == false){
-            ArrayList<CorpsCeleste> l_scan = Thomas.getVaisseau().scanner(u, Thomas, 100);
-            turn.jouerTour(u,Thomas);
+        while(univ.getAstronaute().getVivant()==true && univ.getAstronaute().getTrouvePlanete() == false){
+            ArrayList<CorpsCeleste> l_scan = univ.getAstronaute().getVaisseau().scanner(univ, univ.getAstronaute(), 100);
+            turn.jouerTour(univ,univ.getAstronaute());
             System.out.println("\n");
         }
     }
